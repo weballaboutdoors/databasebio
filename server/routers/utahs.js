e452d5088d0e2620affd3df0dd6bf66bb0d81298
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2022-10-12 15:30:15 -0500

    route complete

---
+++ server/routers/utahs.js
@@ -0,0 +1,74 @@
+const { Router } = require("express");
+const Utah = require("../models/Utah");
+
+const router = Router();
+
+// Create record in MongoDB Atlas using Mongoose.js ORM
+router.post("/", (request, response) => {
+  const newUtah = new Utah(request.body);
+  newUtah.save((error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+// Get (read) all records from the collection
+router.get("/", (request, response) => {
+    Utah.find({}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+// Get a single record by ID using a query parameter
+router.get("/:id", (request, response) => {
+    Utah.findById(request.params.id, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+router.delete("/:id", (request, response) => {
+    Utah.findByIdAndRemove(request.params.id, {}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+router.put("/:id", (request, response) => {
+  const body = request.body;
+  Utah.findByIdAndUpdate(
+    request.params.id,
+    {
+      $set: {
+        name: body.name,
+        companyName: body.company,
+        address: body.street,
+        city: body.city,
+        state: body.state,
+        zipcode: body.zip,
+        phone: body.fone,
+        email: body.mail
+      }
+    },
+    {
+      new: true,
+      upsert: true
+    },
+    (error, record) => {
+      if (error) return response.status(500).json(error);
+      return response.json(record);
+    }
+  );
+});
+
+router.get("/:atrib/:value", (request, response) => {
+  let filter = {};
+  filter[request.params.atrib] = request.params.value;
+  Utah.find(filter, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+module.exports = router;
---
+++ server/routers/wyomings.js
@@ -0,0 +1,74 @@
+const { Router } = require("express");
+const Wyoming = require("../models/Wyoming");
+
+const router = Router();
+
+// Create record in MongoDB Atlas using Mongoose.js ORM
+router.post("/", (request, response) => {
+  const newWyoming = new Wyoming(request.body);
+  newWyoming.save((error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+// Get (read) all records from the collection
+router.get("/", (request, response) => {
+    Wyoming.find({}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+// Get a single record by ID using a query parameter
+router.get("/:id", (request, response) => {
+    Wyoming.findById(request.params.id, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+router.delete("/:id", (request, response) => {
+    Wyoming.findByIdAndRemove(request.params.id, {}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+router.put("/:id", (request, response) => {
+  const body = request.body;
+  Wyoming.findByIdAndUpdate(
+    request.params.id,
+    {
+      $set: {
+        name: body.name,
+        companyName: body.company,
+        address: body.street,
+        city: body.city,
+        state: body.state,
+        zipcode: body.zip,
+        phone: body.fone,
+        email: body.mail
+      }
+    },
+    {
+      new: true,
+      upsert: true
+    },
+    (error, record) => {
+      if (error) return response.status(500).json(error);
+      return response.json(record);
+    }
+  );
+});
+
+router.get("/:atrib/:value", (request, response) => {
+  let filter = {};
+  filter[request.params.atrib] = request.params.value;
+  Wyoming.find(filter, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+module.exports = router;
