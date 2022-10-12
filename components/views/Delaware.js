0149d3093b9476028014e3bdc35769986197d716
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2022-10-12 10:27:31 -0500

    views

--- components/views/Delaware.js
+++ components/views/Delaware.js
@@ -2,4 +2,25 @@ import html from "html-literal";
 
 export default (state) => html `
 <p>this is the delaware page</p>
+<table class="tables">
+    <tr>
+      <th>Name</th>
+      <th>Company Name</th>
+      <th>Address</th>
+      <th>City</th>
+      <th>State</th>
+      <th>Zip Code</th>
+      <th>Phone Number</th>
+      <th>E-Mail</th>
+    </tr>
+    ${state.delawares
+      .map(delaware => {
+        return `<tr><td>${delaware.name}</td><td>${delaware.companyName}</td><td>${delaware.address}</td><td>${delaware.city}</td><td>${delaware.state
+        }</td><td>${delaware.zipcode
+        }</td><td>${delaware.phone
+        }</td><td>${delaware.email
+        }</td></tr>`;
+      })
+      .join("")}
+  </table>
 `;
