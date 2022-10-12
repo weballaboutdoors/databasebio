d5aedfd4731a42ad94f5cd673e5d2621ae1c9537
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2022-10-12 14:28:09 -0500

    models complete

---
+++ server/models/Vermont.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const vermontSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  companyName: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  address: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  city: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  state: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  zipcode: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  phone: {
+    type: String,
+    required: true,
+  },  
+  email: {
+    type: String,
+    required: true,
+  }
+});
+
+const Vermont = mongoose.model("Vermont", vermontSchema);
+
+module.exports = Vermont;
---
+++ server/models/Westvirginia.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const westvirginiaSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  companyName: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  address: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  city: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  state: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  zipcode: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  phone: {
+    type: String,
+    required: true,
+  },  
+  email: {
+    type: String,
+    required: true,
+  }
+});
+
+const Westvirginia = mongoose.model("Westvirginia", westvirginiaSchema);
+
+module.exports = Westvirginia;
