3d06fca668994b7c33a86776990a7e6473e39835
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2022-10-12 09:51:19 -0500

    correcting view

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import html from "html-literal";
 export default state => html`
   <header>
     <div class="container justify-content-start">
-  <h1 class><a href="/Home">InstallerBio</a></h1>
+  <h1><a href="/Home">InstallerBio</a></h1>
   </div>
     <h2 class="headerClass">${state.header}</h2>
   </header>
