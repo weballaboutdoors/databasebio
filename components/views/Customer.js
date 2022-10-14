208c273a765ed2daaa44ced869a84346d3f60318
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2022-10-14 10:12:12 -0500

    updating some css and forms

--- components/views/Customer.js
+++ components/views/Customer.js
@@ -2,23 +2,33 @@ import html from "html-literal";
 
 export default () => html `
 <h1>This is where customers will give feedback</h1>
+<div class="container">
 <form
+  class="formLayout"
   action="https://formspree.io/f/xaykjlab"
   method="POST"
 >
+<div class="formText">
 <label>
     Your Name:
-    <input type="name" name="name">
+    <input type="name" name="name" placeholder="John Doe">
 </label>
+</div>
+<div class="formText">
 <label>
     Your E-Mail:
-    <input type="email" name="email">
+    <input type="email" name="email" placeholder="johndoe@gmail.com">
   </label>
+  </div>
+  <div class="formText">
   <label>
     Your Feedback:
     <textarea name="message"></textarea>
   </label>
-  <!-- your other form fields go here -->
+  </div>
+  <div class="formText">
   <button type="submit">Send</button>
+  </div>
 </form>
+</div>
 `;
--- components/views/Installer.js
+++ components/views/Installer.js
@@ -2,27 +2,64 @@ import html from "html-literal";
 
 export default () => html `
 <h1>This is the installer application Page</h1>
+<div class="container">
 <form
+class="formLayout"
   action="https://formspree.io/f/mlevnoap"
   method="POST"
 >
+<div class="formText">
 <label>
     Your Name:
-    <input type="name" name="name">
+    <input type="name" name="name" placeholder="John Doe">
 </label>
+</div>
+<div class="formText">
+<label>
+    Your Company Name:
+    <input type="compnayName" name="companyName" placeholder="John Doe Installers">
+</label>
+</div>
+<div class="formText">
 <label>
     Your Phone Number:
-    <input type="tel" name="phone">
+    <input type="tel" name="phone" placeholder="8888888888">
 </label>
+</div>
+<div class="formText">
   <label>
     Your email:
-    <input type="email" name="email">
+    <input type="email" name="email" placeholder="jdoe@email.com">
   </label>
+  </div>
+  <div class="formText">
   <label>
-    Your Bio:
-    <textarea name="message"></textarea>
+    Your Address:
+    <textarea name="message" placeholder="123 Main Street"></textarea>
   </label>
+  </div>
+  <div class="formText">
+  <label>
+    Your City:
+    <textarea name="message" placeholder="Cityville"></textarea>
+  </label>
+  </div>
+  <div class="formText">
+  <label>
+    Your State:
+    <textarea name="message" placeholder="Missouri"></textarea>
+  </label>
+  </div>
+  <div class="formText">
+  <label>
+    Your Zip Code:
+    <textarea name="message" placeholder="54321"></textarea>
+  </label>
+  </div>
   <!-- your other form fields go here -->
+  <div class="formText">
   <button type="submit">Send</button>
+  </div>
 </form>
+</div>
 `;
