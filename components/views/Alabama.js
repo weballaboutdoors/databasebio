e0fdc3524b69e92284cd128cc58e31dd15948776
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2022-10-14 14:46:07 -0500

    adding search in bios

--- components/views/Alabama.js
+++ components/views/Alabama.js
@@ -1,9 +1,8 @@
 import html from "html-literal";
 
 export default (state) => html `
-<p>this is the alabama page</p>
-<input id="searchbarName" onkeyup="search_name()" type="text"
-        name="search" placeholder="Search Names..">
+<input id="searchbarCity" onkeyup="search_city()" type="text"
+        name="search" placeholder="Search by City..">
 <input id="searchbarZip" onkeyup="search_zip()" type="text"
         name="search" placeholder="Search by Zip Code..">
 <table class="tables" id="myTable">
@@ -19,8 +18,8 @@ export default (state) => html `
     </tr>
     ${state.alabamas
       .map(alabama => {
-        return `<tr><td class="name">${alabama.name}</td><td>${alabama.companyName}</td><td>${alabama.address}</td><td>${alabama.city}</td><td>${alabama.state
-        }</td><td class="zip">${alabama.zipcode
+        return `<tr><td>${alabama.name}</td><td>${alabama.companyName}</td><td>${alabama.address}</td><td>${alabama.city}</td><td>${alabama.state
+        }</td><td>${alabama.zipcode
         }</td><td>${alabama.phone
         }</td><td>${alabama.email
         }</td></tr>`;
--- zip.js
+++ zip.js
@@ -22,7 +22,7 @@ function search_zip() {
   
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
+      td = tr[i].getElementsByTagName("td")[5];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
