d212f63558decd3ea3327e8f905b3503752a340f
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2022-10-17 14:51:49 -0500

    another form update

--- components/views/Customer.js
+++ components/views/Customer.js
@@ -1,33 +1,19 @@
 import html from "html-literal";
 
 export default () => html `
-<div class="container">
+<div class="formPage">
 <form
   class="formLayout"
   action="https://formspree.io/f/xaykjlab"
   method="POST"
 >
-<div class="formText">
-<label>
-    Your Name:
-    <input type="name" name="name" placeholder="John Doe">
-</label>
-</div>
-<div class="formText">
-<label>
-    Your E-Mail:
-    <input type="email" name="email" placeholder="johndoe@gmail.com">
-  </label>
-  </div>
-  <div class="formText">
-  <label>
-    Your Feedback:
-    <textarea name="message"></textarea>
-  </label>
-  </div>
-  <div class="formText">
-  <button type="submit">Send</button>
-  </div>
+<label for="name">Name</label>
+<input type="text" id="name" name="name" placeholder="Your name">
+<label for="email">E-Mail</label>
+<input type="text" id="email" name="email" placeholder="jdoe@email.com">
+<label for="feedback">Feedback</label>
+<input type="text" id="feedback" name="feedback" placeholder="Your response...">
+    <input type="submit" value="Submit">
 </form>
 </div>
 `;
