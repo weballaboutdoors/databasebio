232dff1c107c176688957ce315add8d4e523e73d
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2022-10-21 12:59:49 -0500

    updating nav bar

--- components/Nav.js
+++ components/Nav.js
@@ -1,14 +1,15 @@
 import html from "html-literal";
 
+
 export default links => html`
-  <nav>
+  <nav class="fixed-nav-bar">
     <i class="fas fa-bars"></i>
-    <ul class="hidden--mobile nav-links">
     <h1><a href="/Home">InstallerBio</a></h1>
+    <ul class="hidden--mobile nav-links">
       ${links
         .map(
           link =>
-            `<li><a href="/${link.title}" title ="${link.title}" data-navigo>${link.text}</a></li>`
+            `<li><a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a></li>`
         )
         .join("")}
     </ul>
