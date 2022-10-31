1bd977bfb0f395405b825b44bf2d292c1d30b624
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2022-10-31 15:29:37 -0500

    final monday update

--- components/views/Home.js
+++ components/views/Home.js
@@ -10,7 +10,9 @@ export default () => html `
         <p>Installer Bio is the window and door installation destination, after purchasing new parts search this page for installers local to you! Installers can create their free account <a href="/Installer">here</a></p>
         </div>
 </div>
-<button class="homeButton"><span class="shadow"></span><span class="edge"></span><a href="/States"><b><span class="homeLayer">Your State Search</span></b></a></button>
-<button class="homeButton"><span class="shadow"></span><span class="edge"></span><a href="/Customer"><b><span class="homeLayer">Customer Feedback</b></a></button>
+<div>
+<button class="homeButton"><span class="shadow"></span><span class="edge"></span><a href="/States"><b><span class="homeLayer">Start Your State Search</span></b></a></button>
 <button class="homeButton"><span class="shadow"></span><span class="edge"></span><a href="/Installer"><b><span class="homeLayer">Installer Application</span></b></a></button>
+<button class="homeButton"><span class="shadow"></span><span class="edge"></span><a href="/Customer"><b><span class="homeLayer">Customer Feedback</b></a></button>
+</div>
 `;
