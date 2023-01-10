8a213598b2d7c490fdc1aed96e00d2122dc7a0d3
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2023-01-10 15:04:53 -0600

    terms update

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default () => html `
 <a href="/Home">Home</a> |
 <a href="https://www.allaboutdoors.com/Products" target="_blank" rel="noopener noreferrer">Find Products</a> 
   |
-  <a href="/Terms">Terms of Conditions</a>|
+  <a href="/Terms">Terms and Conditions</a>|
   <a href="/Privacy">Privacy</a>
   
 </p>
