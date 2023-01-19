ee7848276f748d25f2b670a8f9e20b4ac75876a7
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2023-01-19 11:46:17 -0600

    form update 2

--- server/models/Installer.js
+++ server/models/Installer.js
@@ -3,38 +3,47 @@ const mongoose = require("mongoose");
 const installerSchema = new mongoose.Schema({
   companynameSubmission: {
     type: String,
+    required: true,
     validate: /^[A-Za-z0-9 ]*$/
   },
   addressSubmission: {
     type: String,
+    required: true,
     validate: /^[A-Za-z0-9 ]*$/
   },
   citySubmission: {
     type: String,
+    required: true,
     validate: /^[A-Za-z0-9 ]*$/
   },
   stateSubmission: {
     type: String,
+    required: true,
     enum: ["alabama", "alaska", "arizona", "arkansas", "california", "colorado", "delaware", "districtofcolumbia", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "newhampshire", "newjersey", "newmexico", "newyork", "northcarolina", "northdakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhodeisland", "southcarolina", "southdakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "westvirginia", "wisconsin"]
   },
   zipcodeSubmission: {
     type: String,
+    required: true,
     validate: /^[A-Za-z0-9 ]*$/
   },
   phoneSubmission: {
-    type: String
+    type: String,
+    required: true
   },  
   emailSubmission: {
-    type: String
+    type: String,
+    required: true
   },
   bioSubmission: {
-    type: String
+    type: String,
+    required: true
   },
   webSubmission: {
     type: String,
   },
   reviewSubmission: {
-    type: String
+    type: String,
+    required: true
   },
   professionSubmission: [String]
 });
