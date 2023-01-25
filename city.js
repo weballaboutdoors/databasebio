e08c012875c636119bcdc2c2585c20d17ccb619a
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2023-01-25 14:03:25 -0600

    touch ups

--- city.js
+++ city.js
@@ -1,17 +1,3 @@
-// function search_name() {
-//     let input = document.getElementById('searchbarName').value
-//     input=input.toLowerCase();
-//     let x = document.getElementsByClassName('name');
-      
-//     for (i = 0; i < x.length; i++) { 
-//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
-//             x[i].style.display="none";
-//         }
-//         else {
-//             x[i].style.display="list-item";                 
-//         }
-//     }
-// }
 function search_city() {
     // Declare variables
     var input, filter, table, tr, td, i, txtValue;
