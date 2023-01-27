2cd1ca8311b6940221846184478c35ed36f40806
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2023-01-27 10:55:37 -0600

    adding installer feedback

---
+++ components/views/Feedback.js
@@ -0,0 +1,15 @@
+import html from "html-literal";
+
+export default () => html `
+<head>
+<meta name="description" content="installerbio, door repair, window repair, window installer, door installer, window part installer, door part installer, handyman, door help, window help, customer feedback, customer, customer reply">
+    <meta name="keywords" content="customer, installer bio, installerbio, door installation, window installation, window, door">
+    <meta name="author" content="Delfino Farfan">
+    <title>Installer Feedback</title>
+</head>
+<div class="container">
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfr5Tls0x_vT8Z-khEwWtVAnegcuJyESn6CugTumC1ZLJUxMw/viewform?embedded=true" width="640" height="1010" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+</div>
+
+
+`;
