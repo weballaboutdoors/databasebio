14a8fe14876a6f21b254e2ecf1eec86b592c7cf2
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2023-04-12 10:55:58 -0500

    updating submission page and cache settings

--- components/views/Home.js
+++ components/views/Home.js
@@ -8,13 +8,10 @@ export default (state) => html `
 <title>${state.view}</title>
 </head>
 <div class="container text-center">
-<div class="justify-content-center">
 
-
-        <h2>Welcome to InstallerBio!</h2>
+        <h2>Find Your Installer</h2>
         <h6>From All About Doors and Windows</h6>
         <p>InstallerBio is the window and door installation destination, after purchasing new parts search this page for installers local to you! Create your free <a href="/Installer">InstallerBio profile page</a>!</p>
-        </div>
 </div>
 <p>
 <button class="homeButton"><span class="shadow"></span><span class="edge"></span><a href="/States"><b><span class="homeLayer">Start Your Installer Search</span></b></a></button>
