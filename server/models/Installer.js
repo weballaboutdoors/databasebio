5ebb3595dae10635e01a833387e190a226f225d9
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2023-04-19 15:39:56 -0500

    adding back requirements

--- server/models/Installer.js
+++ server/models/Installer.js
@@ -3,40 +3,40 @@ const mongoose = require("mongoose");
 const installerSchema = new mongoose.Schema({
   companynameSubmission: {
     type: String,
-    // required: true,
+    required: true,
     validate: /^[A-Za-z0-9 ]*$/
   },
   addressSubmission: {
     type: String,
-    // required: true,
+    required: true,
     validate: /^[A-Za-z0-9 ]*$/
   },
   citySubmission: {
     type: String,
-    // required: true,
+    required: true,
     validate: /^[A-Za-z0-9 ]*$/
   },
   stateSubmission: {
     type: String,
-    // required: true,
+    required: true,
     enum: ["empty", "alabama", "alaska", "arizona", "arkansas", "california", "colorado", "delaware", "districtofcolumbia", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "newhampshire", "newjersey", "newmexico", "newyork", "northcarolina", "northdakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhodeisland", "southcarolina", "southdakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "westvirginia", "wisconsin"]
   },
   zipcodeSubmission: {
     type: String,
-    // required: true,
+    required: true,
     validate: /^[A-Za-z0-9 ]*$/
   },
   phoneSubmission: {
     type: String,
-    // required: true
+    required: true
   },  
   emailSubmission: {
     type: String,
-    // required: true
+    required: true
   },
   bioSubmission: {
     type: String,
-    // required: true
+    required: true
   },
   otherSubmission: {
     type: String,
@@ -46,7 +46,7 @@ const installerSchema = new mongoose.Schema({
   },
   yearSubmission: {
     type: String,
-    // required: true
+    required: true
   },
   instypeSubmission: {
     type: String
@@ -56,17 +56,17 @@ const installerSchema = new mongoose.Schema({
   },
   insuranceSubmission: {
     type: String,
-    // required: true,
+    required: true,
     enum: ["yes", "no", "empty"]
   },
   licenseSubmission: {
     type: String,
-    // required: true,
+    required: true,
     enum: ["yes", "no", "empty"]
   },
   reviewSubmission: {
     type: String,
-    // required: true,
+    required: true,
     enum: ["yes", "no", "empty"]
   },
   professionSubmission: [String]
