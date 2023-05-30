dc0a7c3f2ece789a20253ab7bf477add41e24189
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2023-05-30 11:00:24 -0500

    adding KS app

---
+++ components/views/64761b56fcbad7b9c8430dff.js
@@ -0,0 +1,73 @@
+import html from "html-literal";
+
+export default () => html `
+<head>
+<meta name="description" content="The Glass Guru of Olathe, installerbio, door repair, window repair, window installer, door installer, window part installer, door part installer, handyman, door help, window help, search states">
+    <meta name="keywords" content="The Glass Guru of Olathe, state search installer bio, installerbio, door installation, window installation, window, door">
+    <meta name="author" content="Delfino Farfan">
+    <title>The Glass Guru of Olathe</title>
+</head>
+<div class="container">
+<button onclick="history.back()"></button>
+<div class="container text-center">
+<!-- Company name -->
+<h1>The Glass Guru of Olathe</h1>
+<hr class="spacer">
+</div>
+
+<div class="container text-center">
+<!-- Service(s) offered -->
+<h4>Window Part Installer | Door Part Installer | Handyman | Glass Contractor | Window Repair and Service | Door Repair and Service</h4>
+<hr class="spacer">
+</div>
+
+<div class="container">
+<div class="grid-container">
+<div class="item1 item1Border">
+<h4>Contact Info</h4>
+<!-- Company Website -->
+<p><a href="https://www.theglassguru.com/olathe-ks/">https://www.theglassguru.com/olathe-ks/</a></p>
+<!-- Phone Number -->
+<p><a href="tel:+9137128292">(913)712-8292</a></p>
+<!-- Email -->
+<p><a href="mailto:Olathe@theglassguru.com">Olathe@theglassguru.com</a></p>
+<!-- Address -->
+<p>713 N Lindenwood Dr Unit E<br> Olathe, Kansas<br> 66062<br></p>
+</div>
+<div class="item2 item2Border">
+<!-- Installer Bio -->
+<p>The Glass Guru is your local one-stop shop for Glass Repair, Mirrors, Shower Enclosures, Windows, Doors, Screens, Hardware, and more!
+
+Our teams of glass professionals will offer you the broadest range of solutions to address your individual needs, including niche specialty services such as Foggy Window Repair, In-Glass Pet Doors & Custom Mirror Frames, that you simply won't find anywhere else or with any other glass company.</p>
+</div>
+</div>
+<hr class="spacer">
+<!-- Cusomer Reviews -->
+<!-- <h4>Customer Reviews</h4>
+<div class="grid-container">
+
+    <div class="item3">
+<div class="reviewBox">
+<h6></h6>
+<p></p>
+</div>
+<div class="reviewBox">
+<h6></h6>
+<p></p>
+</div>
+
+</div>
+<div class="item4">
+<div class="reviewBox">
+<h6></h6>
+<p></p>
+</div>
+<div class="reviewBox">
+<h6></h6>
+<p></p>
+</div> -->
+</div>
+</div>
+</div>
+</div>
+`;
--- components/views/index.js
+++ components/views/index.js
@@ -59,4 +59,5 @@ export { default as Terms } from "./Terms";
 export { default as Privacy } from "./Privacy";
 export { default as Update } from "./Update";
 export { default as Feedback } from "./Feedback";
-export {default as P6475f92fac17a20d9c911e6d } from "./6475f92fac17a20d9c911e6d";
+export { default as P6475f92fac17a20d9c911e6d } from "./6475f92fac17a20d9c911e6d";
+export { default as P64429857055bfd617330f85a } from "./64429857055bfd617330f85a";
