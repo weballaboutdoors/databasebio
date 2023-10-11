f4da8ca98b2e8d7cbba2e5f4765ee16e2b182108
devon-plackard-aad <74269626+devon-plackard-aad@users.noreply.github.com>
2023-10-11 14:33:56 -0500

    uploading kansas and nevada installer

---
+++ components/views/6526f35fdcd1cae554aa28c5.js
@@ -0,0 +1,81 @@
+import html from "html-literal";
+
+export default () => html `
+<head>
+<meta name="description" content="Your Handy Man, installerbio, door repair, window repair, window installer, door installer, window part installer, door part installer, handyman, door help, window help, search states">
+    <meta name="keywords" content="Your Handy Man, state search installer bio, installerbio, door installation, window installation, window, door">
+    <meta name="author" content="Delfino Farfan">
+    <title>Your Handy Man</title>
+</head>
+<div class="container">
+<button onclick="history.back()"></button>
+<div class="container text-center">
+<!-- Company name -->
+<h1>Your Handy Man</h1>
+<hr class="spacer">
+</div>
+
+<div class="container text-center">
+<!-- Service(s) offered -->
+<h4>Handyman | Window Repair and Service | Door Repair and Service | Locksmith</h4>
+<hr class="spacer">
+</div>
+
+<div class="container">
+<div class="grid-container">
+<div class="item1 item1Border">
+<h4>Contact Info</h4>
+<!-- Company Website -->
+<!-- <p><a href="https://www.star-ws.com/">https://www.star-ws.com/</a></p> -->
+<!-- Phone Number -->
+<p><a href="tel:+9135265440">(913)526-5440</a></p>
+<!-- Email -->
+<p><a href="mailto:dmillerop@gmail.com">dmillerop@gmail.com</a></p>
+<!-- Address -->
+<p><a href="https://www.google.com/maps/dir//12308+Beverly+St,+Leawood,+KS+66209/@39.0212743,-94.7389639,11z/data=!4m9!4m8!1m0!1m5!1m1!1s0x87c0ea0560a5c45b:0xbe6adf5f46864ffa!2m2!1d-94.6550908!2d38.9042389!3e0?entry=ttu">12308 Beverly St<br> Overland Park, Kansas<br> 66209<br></a></p>
+</div>
+<div class="item2 item2Border">
+<!-- Installer Bio -->
+<p>I am a former home builder who has always helped friends and neighbors with projects and repairs around the house. Several years ago I opened my own company and have received most jobs through word of mouth ever since.<br>
+<br> 
+<b>Years of Experience:</b> 32 Years
+<br><br> 
+<b>Does your company have Insurance?</b> Yes &#x2713;
+<br><br>
+<b>Type of Insurance?</b> General Liability 
+<br><br>
+<b>Does your company have a license?</b> No &#x2717;
+<br><br>
+<!-- <b>Type of Certification?</b> Contact Installer for details</p> -->
+</div>
+</div>
+<hr class="spacer">
+<!-- Cusomer Reviews -->
+<!-- <h4>Customer Reviews</h4>
+<div class="grid-container">
+
+    <div class="item3">
+<div class="reviewBox">
+<h6></h6>
+<p></p>
+</div>
+<div class="reviewBox">
+<h6></h6>
+<p></p>
+</div>
+
+</div>
+<div class="item4">
+<div class="reviewBox">
+<h6></h6>
+<p></p>
+</div>
+<div class="reviewBox">
+<h6></h6>
+<p></p>
+</div> -->
+</div>
+</div>
+</div>
+</div>
+`;
